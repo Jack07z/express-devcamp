317e691ee6a420d88da768c002c07e2434870843
Your Name <you@example.com>
2022-10-19 09:39:12 -0500

    rutas del proecto

---
+++ server.js
@@ -0,0 +1,35 @@
+//1. crear el objeto express
+const express = require('express')
+//2. citar las dependencias necesarias para el proyecto 
+const dotenv = require('dotenv')
+const colors = require('colors')
+const listEndpoint = require('express-list-endpoints')
+//las rutas de ruta 
+const bootcampRoutes = require('./routes/BootcampRoutes') 
+
+
+//3. establecer archivo de configuracion 
+dotenv.config({
+    path:'./config/config.env'
+})
+//console.log(process.env.PORT)
+
+//Crear el objeto aplicacion 
+//para el servidor de desarrollo 
+const app = express()
+
+//rutas de proyecto 
+app.use('/api/v1/bootcamps' , bootcampRoutes)
+
+//rutas de aplicacion 
+app.get('/' , (request , response )=>{
+    response
+    .status(200)
+    .json({
+        "success": true,
+        "data" : "request exitosa"
+    })
+})
+app.listen(process.nextTick.PORT , ()=>{
+    console.log(`servidor activo en puerto 5000`.bgGreen)
+})
