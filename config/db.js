d3f271a67c5a3f8b6d6ac5646e67ca03739818cf
Your Name <you@example.com>
2022-10-27 09:29:45 -0500

    rutas de user

--- config/db.js
+++ config/db.js
@@ -1,22 +1,12 @@
 const sequelize = require('./seq')
 const colors = require('colors')
-const {DataTypes} = require('sequelize')
-
-//crear instancia de el modelo User
-const UserModel = require('../models/user')
-const User = UserModel(sequelize, DataTypes)
-
-//definir la funcion de conexion a la base de datos
-
 
 const connectDB = async ()=>{
     try{
     //conectarse a la base de datos 
     await sequelize.authenticate()
     console.log('Conectado a mysql'.bgRed)
-    const jane = await User.create
-    ({ username: "Jack", email: "jogonzale48♠4misena.edu.co", password:"123456" });
-    console.log("Jane's auto-generated ID:", jane.id);
+
     }
     catch(error)
     {
@@ -24,4 +14,4 @@ const connectDB = async ()=>{
     }
 } 
 
-connectDB()
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 //1. crear el objeto express
 const express = require('express')
+
 //2. citar las dependencias necesarias para el proyecto 
 const dotenv = require('dotenv')
 const colors = require('colors')
+const connectDB = require('./config/db')
 const listEndpoint = require('express-list-endpoints')
+
 //las rutas de ruta 
 const bootcampRoutes = require('./routes/BootcampRoutes') 
 const CoursesRoute = require('./routes/CoursesRoute') 
+const UserRoutes = require('./routes/UserRoutes') 
 
 //3. establecer archivo de configuracion 
 dotenv.config({
@@ -17,11 +21,21 @@ console.log(process.env.PORT)
 //Crear el objeto aplicacion 
 //para el servidor de desarrollo 
 const app = express()
+//validemos el objeto application
+//para recibir datos en formato json
+app.use(express.json())
+
+//conexion a bd
+connectDB()
 
 //rutas de proyecto 
 app.use('/api/v1/Courses' , CoursesRoute)
+app.use('/api/v1/users', UserRoutes)
 //rutas de proyecto 
 app.use('/api/v1/bootcamps' , bootcampRoutes)
+
+//imprimir la lista de endpoints validas del proyecto
+console.log(listEndpoint(app))
 
 //rutas de aplicacion 
 app.get('/' , (request , response )=>{
